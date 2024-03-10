f968aa60a52c2dade3a664df9be8e03868caa084
m-muiz <mmuizsiddiqui@gmail.com>
2024-03-10 22:48:45 +0500

    complete response section

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import Header from "./components/Header/Header"
 import Hero from "./components/Hero/Hero"
 import NextStep from "./components/NextStep/NextStep"
+import Response from "./components/Response/Response"
 
 const App = () => {
 
@@ -9,6 +10,7 @@ const App = () => {
       <Header />
       <Hero />
       <NextStep />
+      <Response />
     </>
 
   )
---
+++ src/components/Response/Response.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const Response = () => {
+    return (
+        <div className='flex items-center gap-32 max-w-6xl mx-auto mt-24'>
+            <div className="h-full">
+                <img src="https://assets.website-files.com/5f0a3ac2c99ebfc3a7c8728b/5f0a3b1040228a02efb5ed12_asset%203.png" alt="" />
+            </div>
+            <div className="flex flex-col gap-10">
+                <h1 className="text-6xl font-bold">
+                    Response on a chat<br /> wherever you are
+                </h1>
+
+                <p className="text-base">
+                    Create custom landing pages with Shades that<br /> convert more visitors than any website, no<br /> coding required.
+                </p>
+
+                <h5 className='text-[#1290a4] cursor-pointer'>Learn More &gt;</h5>
+            </div>
+        </div>
+    )
+}
+
+export default Response
