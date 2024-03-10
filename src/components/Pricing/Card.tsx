50d3510b5247d35e106d94e556dd29e0e8d86d73
m-muiz <mmuizsiddiqui@gmail.com>
2024-03-11 00:25:17 +0500

    complete

--- src/components/Pricing/Card.tsx
+++ src/components/Pricing/Card.tsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import check from "../../assets/blackCheck.png"
 
-const Card = () => {
+const Card = ({ data, ind }: any) => {
     return (
-        <div className='bg-white p-4'>
-
+        <div className="bg-white p-8 w-[360px] rounded-md flex flex-col gap-6 relative" key={ind}>
+            {data.isPopular ? (
+                <div className="absolute top-0 left-0 right-0 bg-red-700 w-full h-2" />
+            ) : ("")}
+            <h5 className="relative">{data.name}
+                {
+                    data.isPopular ? (
+                        <div className="bg-red-300/90 p-2 max-w-max rounded-[100px] absolute top-0 right-1 text-sm text-white font-semibold">Popular</div>
+                    ) : ("")
+                }
+            </h5>
+            <h2 className='text-2xl font-semibold'>${data.price} <span className='text-sm'>/month</span></h2>
+            <p className='text-sm'>Good for small business launching their products less then once a year</p>
+            <div className="flex items-center gap-2">
+                <img className="h-3 w-3 object-contain" src={check} alt="check" />
+                <h4 className={`${data.blocks ? "" : "line-through"}`}>Unlimited Blocks</h4>
+            </div>
+            <div className="flex items-center gap-2">
+                <img className="h-3 w-3 object-contain" src={check} alt="check" />
+                <h4 className={`${data.cloud ? "" : "line-through"}`}>5GB Clould Storages</h4>
+            </div>
+            <div className="flex items-center gap-2">
+                <img className="h-3 w-3 object-contain" src={check} alt="check" />
+                <h4 className={`${data.domain ? "" : "line-through"}`}>Custom Domain Names</h4>
+            </div>
+            <div className="flex items-center gap-2">
+                <img className="h-3 w-3 object-contain" src={check} alt="check" />
+                <h4 className={`${data.emails ? "" : "line-through"}`}>Unlimited Emails</h4>
+            </div>
+            <div>
+                <button className="bg-[#1290a4] text-white p-3 w-36 font-semibold tracking-wider rounded-md text-sm h-12">Start free trial</button>
+                <p className="text-sm">No credit card required</p>
+            </div>
         </div>
     )
 }
--- src/components/Pricing/Pricing.tsx
+++ src/components/Pricing/Pricing.tsx
@@ -2,15 +2,50 @@ import pricingBorder from "../../assets/pricingBorder.svg"
 import Card from "./Card"
 
 const Pricing = () => {
+  const data = [
+    {
+      name: "BASIC",
+      price: 19,
+      blocks: true,
+      cloud: true,
+      domain: false,
+      emails: false,
+      isPopular: false
+    },
+    {
+      name: "PRO",
+      price: 25,
+      blocks: true,
+      cloud: true,
+      domain: true,
+      emails: false,
+      isPopular: true
+    },
+    {
+      name: "ADVANCE",
+      price: 30,
+      blocks: true,
+      cloud: true,
+      domain: true,
+      emails: true,
+      isPopular: false
+    },
+  ];
   return (
     <div className="mt-20">
       <img src={pricingBorder} alt="pricingBorder" />
-      <div className="bg-[#30393b] -mt-4 pb-10 flex flex-col gap-6">
+      <div className="bg-[#30393b] -mt-4 pb-10 flex flex-col gap-10">
         <h1 className="text-4xl text-center text-white font-semibold">Pricing that suits you better</h1>
         <p className="text-slate-300/80 text-lg text-center">
           We designed and tested prototypes that helped identify pain points in<br /> the account creation process. Together, we shaped the new standard.
         </p>
-        <Card />
+        <div className="flex flex-wrap max-w-6xl mx-auto gap-4">
+          {
+            data.map((item, ind) => (
+              <Card data={item} ind={ind} />
+            ))
+          }
+        </div>
         <p className="text-slate-300/80 text-lg text-center">
           Still confused?
           <span className="text-white hover:underline ml-3 cursor-pointer">
