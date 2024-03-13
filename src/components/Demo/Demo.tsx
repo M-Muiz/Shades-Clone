5d62a7f6d1f53c8c2374e63c425f4d43f88d580c
m-muiz <mmuizsiddiqui@gmail.com>
2024-03-13 13:16:04 +0500

    working on responsivness

--- src/components/Demo/Demo.tsx
+++ src/components/Demo/Demo.tsx
@@ -2,21 +2,21 @@ import demoImg from "../../assets/demo.png"
 
 const Demo = () => {
     return (
-        <div className='flex justify-between max-w-6xl mx-auto mt-24 pb-6'>
-            <div className="h-full w-full">
+        <div className='flex justify-between max-w-6xl mx-auto mt-24 pb-6 flex-col lg:flex-row items-center gap-4'>
+            <div className="h-full w-2/4">
                 <img src={demoImg} alt="Demo" />
             </div>
-            <div className="flex flex-col gap-7">
-                <h1 className="text-5xl font-bold">
+            <div className="flex flex-col gap-7 px-3 w-2/4">
+                <h1 className="text-2xl lg:text-5xl font-bold text-center lg:text-start">
                     Increase Customer Support Productivity
                 </h1>
 
-                <p className="text-base">
+                <p className="text-base text-center lg:text-start">
                     Event is not like most tech conferences. We<br /> want our presentations to engage the audience,<br /> spark discussion and inspire new ideas.
                 </p>
 
                 <div className="seprator" />
-                <p className="text-sm">
+                <p className="text-sm text-center lg:text-start">
                     Interested to see how it works? Schedule a free demo to get<br /> to learn everything you need to know.
                 </p>
 
--- src/components/Response/Response.tsx
+++ src/components/Response/Response.tsx
@@ -2,12 +2,12 @@ import response from "../../assets/response.png"
 
 const Response = () => {
     return (
-        <div className='flex items-center gap-32 max-w-6xl mx-auto mt-24'>
-            <div className="h-full">
-                <img src={response} alt="response" />
+        <div className='flex items-center gap-8 lg:gap-32 max-w-6xl mx-auto mt-24 flex-col lg:flex-row'>
+            <div className="lg:h-full h-[300px]">
+                <img src={response} alt="response" className="h-full" />
             </div>
             <div className="flex flex-col gap-10">
-                <h1 className="text-6xl font-bold">
+                <h1 className="text-4xl font-bold lg:text-6xl">
                     Response on a chat<br /> wherever you are
                 </h1>
 
